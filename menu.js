6d5e6ebad69e3a2b8f8169dc7f9c06454b5998b0
Mohammed Amine Lahmami <86573912+mohammedaminelahmami@users.noreply.github.com>
2021-12-31 14:46:38 +0100

    p3

---
+++ menu.js
@@ -0,0 +1,6 @@
+let hmb__menu = document.getElementById("hmb__menu");
+let my__nav = document.getElementById("my__nav")
+
+hmb__menu.addEventListener("click", () => {
+	my__nav.classList.toggle("show");
+});
